09cccf368cdf8baed4edc1b80188d477f15cfe60
EnesTopal <enes.topal1@gazi.edu.tr>
2025-08-24 19:08:35 +0300

    creating and dependencies

---
+++ src/main/java/com/tpl/tupalle/TupalleApplication.java
@@ -0,0 +1,14 @@
+package com.tpl.tupalle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TupalleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TupalleApplication.class, args);
+        System.out.println("Runs \uD83D\uDC4D");
+	}
+
+}
---
+++ src/test/java/com/tpl/tupalle/TupalleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpl.tupalle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TupalleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
